85720c333d4921bdaa63ba1f0e535631b3d0c26a
Rohan Prinja <rohan.prinja@gmail.com>
2022-11-30 21:05:16 -0800

    1

---
+++ 1.py
@@ -0,0 +1,8 @@
+with open('1.txt', 'r') as f:
+    data = f.read()
+
+elves = data.strip().split('\n\n')
+elves = [sum(map(int, elf.split('\n'))) for elf in elves] # total for each elf
+elves.sort()
+print(elves[-1]) # highest value
+print(elves[-1] + elves[-2] + elves[-3]) # sum of top 3 values
