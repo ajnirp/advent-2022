ecd2fc73714ee2aeec7b26d74fedbf70080062f9
Rohan Prinja <rohan.prinja@gmail.com>
2022-12-02 21:10:15 -0800

    3

---
+++ 3.py
@@ -0,0 +1,38 @@
+def priority(char):
+    if 'a' <= char <= 'z':
+        return ord(char) - ord('a') + 1
+    return ord(char) - ord('A') + 27
+
+def common2(s1, s2):
+    for c in s1:
+        if c in s2:
+            return c
+
+def to_set(s):
+    return set(c for c in s)
+
+def common3(s1, s2, s3):
+    set1 = to_set(s1)
+    set2 = to_set(s2)
+    set3 = to_set(s3)
+    result = set1.intersection(set2).intersection(set3)
+    for c in result:
+        return c
+
+with open('3.txt', 'r') as f:
+    data = f.readlines()
+    result_1 = 0
+    for line in data:
+        line = line.strip()
+        n = len(line)
+        mid = n//2
+        first, second = line[:mid], line[mid:]
+        result_1 += priority(common2(first, second))
+    print(result_1)
+
+    result_2 = 0
+    # look at 3 lines at a time
+    for i in range(len(data)//3):
+        lines = [line.strip() for line in data[3*i:3*i+3]]
+        result_2 += priority(common3(*lines))
+    print(result_2)
