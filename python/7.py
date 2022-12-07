ec4b5c8b194c9e86a0ed998442890dbd9c7b5395
Rohan Prinja <rohan.prinja@gmail.com>
2022-12-06 23:28:36 -0800

    7

--- python/7.py
+++ python/7.py
@@ -1,5 +1,8 @@
-with open('7.txt', 'r') as f:
-    data = [line.strip() for line in f.readlines()]
+class Constants:
+    CUTOFF = 100_000
+    ROOT_NAME = '/'
+    TOTAL_SPACE = 70_000_000
+    UNUSED_REQUIRED = 30_000_000
 
 # Object representing a file or directory in the filesystem
 class Node:
@@ -15,21 +18,16 @@ class Node:
         return self.path.split('/')[-1]
 
 # Object that holds state as we move around inside the filesystem
-class Config:
-    CUTOFF = 100_000
-    ROOT_NAME = '/'
-    TOTAL_SPACE = 70_000_000
-    UNUSED_REQUIRED = 30_000_000
-
+class State:
     def __init__(self):
         # Root node '/''
-        self.root = Node(Config.ROOT_NAME)
+        self.root = Node(Constants.ROOT_NAME)
 
         # Map of paths to Nodes
-        self.nodes = {Config.ROOT_NAME: self.root}
+        self.nodes = {Constants.ROOT_NAME: self.root}
 
         # Current working directory
-        self.cwd = [Config.ROOT_NAME]
+        self.cwd = [Constants.ROOT_NAME]
 
         # Set of directories with size under CUTOFF. Updated as soon as new data
         # is available rather than on-demand
@@ -51,7 +49,7 @@ class Config:
 
     # Change current working directory to root
     def cd_root(self):
-        self.cwd = [Config.ROOT_NAME]
+        self.cwd = [Constants.ROOT_NAME]
 
     # Change current working directory
     def cd(self, target_dir):
@@ -71,7 +69,7 @@ class Config:
     # Lookup a Node from the map
     def get_node(self, path):
         if self.is_root_path(path):
-            return self.get_node(Config.ROOT_NAME)
+            return self.get_node(Constants.ROOT_NAME)
         return self.nodes[path]
 
     def maybe_create_dir(self, name):
@@ -98,54 +96,56 @@ class Config:
         node.parent = parent
 
         parent.size += size
-        if parent.size > Config.CUTOFF and parent in self.smol_dirs:
+        if parent.size > Constants.CUTOFF and parent in self.smol_dirs:
             self.smol_dirs.remove(parent)
 
         curr_node = parent.parent
         while curr_node:
             curr_node.size += node.size
-            if curr_node.size > Config.CUTOFF and curr_node in self.smol_dirs:
+            if curr_node.size > Constants.CUTOFF and curr_node in self.smol_dirs:
                 self.smol_dirs.remove(curr_node)
             curr_node = curr_node.parent
 
+    # Calculate how much space we need to free up
+    def extra_space_needed(self):
+        return Constants.UNUSED_REQUIRED + self.root.size - Constants.TOTAL_SPACE
+
+    def size_of_smallest_dir_to_delete(self):
+        def preorder_traversal(node, result_array):
+            result_array.append(node)
+            for child in node.children:
+                preorder_traversal(child, result_array)
+        traversal = []
+        preorder_traversal(self.root, traversal)
+
+        space_needed = self.extra_space_needed()
+        min_so_far = self.root.size
+        for node in traversal:
+            if len(node.children) == 0:
+                continue
+            if node.size > space_needed and node.size < min_so_far:
+                min_so_far = node.size
+        return min_so_far
+
 # Process file input
-def process_data(data, config):
+def process_data(data, state):
     for line in data:
         if line[0] == '$':
             instruction = line.split()[1]
             if instruction == 'cd':
                 target_dir = line.split()[-1]
-                config.cd(target_dir)
+                state.cd(target_dir)
         elif line[:3] == 'dir':
             name = line.split()[1]
-            config.maybe_create_dir(name)
+            state.maybe_create_dir(name)
         elif line[0] in '0123456789':
             size, name = line.split()
             size = int(size)
-            config.maybe_create_file(name, size)
+            state.maybe_create_file(name, size)
 
-# Calculate how much space we need to free up
-def extra_space_needed(root):
-    return Config.UNUSED_REQUIRED + root.size - Config.TOTAL_SPACE
-
-def size_of_smallest_dir_to_delete(root):
-    def preorder_traversal(node, result_array):
-        result_array.append(node)
-        for child in node.children:
-            preorder_traversal(child, result_array)
-    traversal = []
-    preorder_traversal(root, traversal)
-
-    space_needed = extra_space_needed(root)
-    min_so_far = root.size
-    for node in traversal:
-        if len(node.children) == 0:
-            continue
-        if node.size > space_needed and node.size < min_so_far:
-            min_so_far = node.size
-    return min_so_far
-
-config = Config()
-process_data(data, config)
-print(sum(d.size for d in config.smol_dirs))
-print(size_of_smallest_dir_to_delete(config.root))
+with open('7.txt', 'r') as f:
+    data = [line.strip() for line in f.readlines()]
+    state = State()
+    process_data(data, state)
+    print(sum(d.size for d in state.smol_dirs))
+    print(state.size_of_smallest_dir_to_delete())
