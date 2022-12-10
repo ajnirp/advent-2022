10d2c19a8d079e93a800823eefc2b538ff8a24b3
Rohan Prinja <rohan.prinja@gmail.com>
2022-12-09 21:50:19 -0800

    10

--- python/10.py
+++ python/10.py
@@ -1,3 +1,5 @@
+ADDX_LEN = 2 # length of an addx cycle
+
 def update(cycle, crt, x):
     pos = cycle - 1
     row = pos // 40
@@ -5,31 +7,35 @@ def update(cycle, crt, x):
     if col in [x-1, x, x+1]:
         crt[row][col] = '#'
 
-def process(instructions):
-    interesting = [20 + 40*i for i in range(6)]
+def part1(instructions):
+    checkpoint = [20 + 40*i for i in range(6)]
     result, idx, x, cycle = 0, 0, 1, 1
 
     for instr in instructions:
-        if idx == len(interesting):
+        # If we've finished all our checkpoints, return.
+        if idx == len(checkpoint):
             return result
-        if cycle == interesting[idx]:
-            result += interesting[idx] * x
+        # We're at a checkpoint. Increase result.
+        if cycle == checkpoint[idx]:
+            result += checkpoint[idx] * x
             idx += 1
         if instr[0] == 'n':
             cycle += 1
             continue
         else:
-            if idx == len(interesting):
+            # If we've finished all our checkpoints, return.
+            if idx == len(checkpoint):
                 return result
-            if cycle + 2 > interesting[idx]:
-                result += interesting[idx] * x
+            # We're going to cross a checkpoint. Increase result.
+            if cycle + ADDX_LEN > checkpoint[idx]:
+                result += checkpoint[idx] * x
                 idx += 1
             val = int(instr.split()[-1])
-            cycle += 2
+            cycle += ADDX_LEN
             x += val
     return result
 
-def render(instructions):
+def part2(instructions):
     x, cycle = 1, 1
     crt = [['.' for _ in range(40)] for _ in range(6)]
     for instr in instructions:
@@ -38,17 +44,15 @@ def render(instructions):
             cycle += 1
             continue
         else:
-            update(cycle, crt, x)
-            cycle += 1
-            update(cycle, crt, x)
-            cycle += 1
+            for _ in range(ADDX_LEN):
+                update(cycle, crt, x)
+                cycle += 1
             val = int(instr.split()[-1])
             x += val
     for row in crt:
         print(''.join(row))
 
-
 with open('../data/10.txt', 'r') as f:
     instructions = [line.strip() for line in f.readlines()]
-    print(process(instructions))
-    render(instructions)
+    print(part1(instructions))
+    part2(instructions)
